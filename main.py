08e874c868ffab8d2d5394c6802bf2fcd6a57709
Duncan Mazza <dmazza@olin.edu>
2019-11-07 01:36:07 -0500

    Change default parameters

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ book_list = (
 
 if __name__ == "__main__":
     # acquire books from the book list
-    librarian = Librarian(book_list, global_truncate=0.4, global_alpha=1, global_max_chain=2)
+    librarian = Librarian(book_list, global_truncate=0.4, global_alpha=1, global_max_chain=15)
     while True:
         for book_name in librarian.acquired_books:
             book = librarian.acquired_books[book_name]
             print("Applying naive bayes to: {}".format(book_name))
-            book.apply_naive_bayes(extend_by=15)
+            book.apply_naive_bayes(extend_by=50)
 
         print('Generate another sample? (y or Y) -> ')
         while True:
