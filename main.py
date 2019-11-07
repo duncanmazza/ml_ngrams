0edbe799a2d54eb746ac37fd9cda49724f7c7ba5
Duncan Mazza <dmazza@olin.edu>
2019-11-06 20:34:13 -0500

    Update main routine to reflect Book class functionality

--- main.py
+++ main.py
@@ -7,12 +7,22 @@ from api.librarian import Librarian
 
 book_list = (
     ('Frankenstein', 'Mary Wollstonecraft (Godwin) Shelley'),
+    # ('Watersprings', 'Arthur Christopher Benson')
 )
 
 if __name__ == "__main__":
     # TODO: add an argument parser
 
     # acquire books from the book list
-    librarian = Librarian(book_list)
-    for book in librarian.acquired_books:
-        book.analyze()
+    librarian = Librarian(book_list, global_truncate=0.4, global_alpha=1, global_max_chain=15)
+    while True:
+        for book_name in librarian.acquired_books:
+            book = librarian.acquired_books[book_name]
+            print("Appling naive bayes to: {}".format(book_name))
+            book.apply_naive_bayes()
+
+        print('Generate another sample? (y or Y) -> ')
+        while True:
+            i = input()
+            if i == "y" or "Y":
+                break   
